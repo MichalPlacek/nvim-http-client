ce54563505cdefcb62bcd53c49ed73d42514d113
michal <michal.placek@yandex.com>
2022-02-14 01:51:22 +0100

    result window

--- lua/http_client/http-client.lua
+++ lua/http_client/http-client.lua
@@ -1,12 +1,14 @@
 local M = {}
 
 local function invoke_request(request)
+  local return_value = {}
   if request["url"] then
     local result = vim.fn.systemlist("curl "..request["url"])
     for k,v in pairs(result) do
-      print(result[k])
+      table.insert(return_value,result[k])
     end
   end
+  return return_value;
 end
 
 local function clean_line(line)
@@ -62,6 +64,25 @@ local function parse_lines(lines)
   return result
 end
 
+local function show_window(lines)
+  local width = vim.api.nvim_get_option("columns")
+  local height = vim.api.nvim_get_option("lines")
+  if(width > 5 and height > 5) then
+    local buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+    vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+    local opts = {
+      style = "minimal",
+      relative = "editor",
+      row = 2,
+      col = 2,
+      width = width -4,
+      height = height -4,
+    }
+    vim.api.nvim_open_win(buf, true, opts)
+  end
+end
+
 function M.invoke()
   print(vim.bo.filetype)
       --  print('Hello world!!')
@@ -82,12 +103,11 @@ local lines = vim.api.nvim_buf_get_lines(0,0,vim.api.nvim_buf_line_count(0),1)
   local request = parse_lines(lines)
   print("Url:")
   print(request["url"])
-  invoke_request(request)
-  local size = table.getn(lines)
-  print(size);
-  for l = 1, size, 1 do
-         print(lines[l])
-  end
+  local result = invoke_request(request)
+  show_window(result);
+  --for l = 1, size, 1 do
+  --       print(lines[l])
+  --end
  -- ivnokeRequest()
 end
 
